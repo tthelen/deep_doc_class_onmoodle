b561565842c3d76ae867b5628dbb34ce8fe3b93c
Tobias Thelen <tt@tobiasthelen.de>
2020-01-28 08:38:43 +0100

    - removed link to old deepdocclass repository - added link to current docker project

--- joinresults.r
+++
@@ -1,5 +0,0 @@
-pred = read.csv("/tmp/tmp/results20282/predictions/prediction_batch0.csv")
-
-files = read.csv("/tmp/tmp/course_20282.csv")
-
-comb = merge(pred,files,by.x = "id", by.y = "document_id")
